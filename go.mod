182ef0286c46f58c900cc524557ef878790526f1
Medit <medit.ding@homepartners.tech>
2021-03-02 23:39:10 +0800

    add demo-go

---
+++ go.mod
@@ -0,0 +1,7 @@
+module demo-go
+
+go 1.14
+
+require (
+	github.com/vsixz/common-go v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vsixz/common-go/hello"
+)
+func main(){
+	fmt.Println("test hello:")
+	hello.Say("Jay")
+}
