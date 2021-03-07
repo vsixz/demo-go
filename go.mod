fad3bc6fdd01f3cb34567fa0d4963c4f7411d1f2
Medit <medit.ding@homepartners.tech>
2021-03-07 22:57:59 +0800

    local

--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ go 1.14
 
 require (
 	github.com/vsixz/common-go v1.0.0
+	github.com/vsixz/common-go/v2 v2.0.0
 )
+
+replace (
+	github.com/vsixz/common-go => ../common-go
+	github.com/vsixz/common-go/v2 => ../common-go/v2
+)
+
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/vsixz/common-go/hello"
+	helloV2 "github.com/vsixz/common-go/v2/hello"
 )
-func main(){
+
+func main() {
 	fmt.Println("test hello:")
 	hello.Say("Jay")
+	helloV2.Say("Jay", "Chou")
 }
